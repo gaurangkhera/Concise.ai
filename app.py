3c661f38ff642d521bda39d3a1f190499f518693
Gaurang <khera.gaurang@gmail.com>
2023-04-28 16:02:58 +0530

    Init

--- app.py
+++ app.py
@@ -1,19 +1,49 @@
 from hack import app, create_db, db
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
-from hack.forms import LoginForm, RegForm
+from hack.forms import LoginForm, RegForm, SummarizeForm
 from hack.models import User
 from werkzeug.security import generate_password_hash, check_password_hash
+import requests
 
 create_db(app)
+headers = {
+	"content-type": "application/octet-stream",
+	"X-RapidAPI-Key": "188adcc90dmsh2c0204da620cc1cp11c251jsn154284079e2b",
+	"X-RapidAPI-Host": "article-extractor-and-summarizer.p.rapidapi.com"
+}
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+@app.route('/summarize', methods=['GET', 'POST'])
+@login_required
+def summarize():
+    form = SummarizeForm()
+    url = "https://article-extractor-and-summarizer.p.rapidapi.com/summarize"
+    result = ''
+    if form.validate_on_submit():
+        query = {
+            'url': form.url.data,
+            'length': 3
+        }
+        print(query)
+        res = requests.get(url, headers=headers, params=query)
+        if res.status_code != 200:
+            result = 'An error occured while trying to summarize this article. Please try another URL.'
+        else:
+            result = res.json()['summary']
+    return render_template('summarize.html', form=form, result=result)
+
+next_list_1 = []
 @app.route('/reg', methods=['GET', 'POST'])
 def reg():
     form = RegForm()
+    next = request.args.get('next')
+    print(next)
+    if next != None:
+        next_list_1.append(next)
     mess=''
     if form.validate_on_submit():
         email = form.email.data
@@ -27,12 +57,19 @@ def reg():
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user)
-            return redirect('/')
+            return redirect('/summarize')
     return render_template('reg.html', form=form, mess=mess)
+
+next_list = []
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    next = request.args.get('next')
+    print(next)
+    if next != None:
+        next_list.append(next)
+    # print(next)
     mess=''
     if form.validate_on_submit():
         email = form.email.data
@@ -43,7 +80,8 @@ def login():
         else:
             if check_password_hash(user.password, password):
                 login_user(user, remember=True)
-                return redirect(url_for('home'))
+                print(next_list)
+                return redirect('/summarize')
             else:
                 mess = 'Incorrect password.'
     return render_template('login.html', mess=mess, form=form)
--- hack/forms.py
+++ hack/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField, IntegerField
+from wtforms import StringField,PasswordField,SubmitField, IntegerField, URLField
 from wtforms.validators import DataRequired, Length
 
 class LoginForm(FlaskForm):
@@ -12,3 +12,7 @@ class RegForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=3, max=32)])
     password = PasswordField('Password',validators=[DataRequired(), Length(min=8, max=128)])
     submit = SubmitField("Register")
+
+class SummarizeForm(FlaskForm):
+    url = URLField('url',validators=[DataRequired()])
+    submit = SubmitField('Summarize!')
